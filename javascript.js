324165a780f079617eb51deecc523ee1d919cd9a
haseeb-farahi <111962155+haseeb-farahi@users.noreply.github.com>
2022-08-29 12:38:19 +0430

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,25 @@
+// var Fname = 'sharif';
+// var Fname = 'shirzad';
+// document.write(Fname)
+// .console.log();
+// var Fname "fayaz nasrati"
+// var First_name_asdf_ ="asee"
+// function myfanc()
+// var FirstName;
+// var FirstName;
+
+// document.getElementById
+
+
+
+
+
+
+// var x = 10;
+// var y = 5;
+
+// var z = x + y;
+//  document.write(z)
+
+
+//  document.getElementById('demo').innerHTML="Hello World";
